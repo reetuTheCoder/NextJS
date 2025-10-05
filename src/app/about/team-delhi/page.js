00376e67ff466410c5ba412918761f8cc1e42401
reetuTheCoder <codewithreetu@gmail.com>
2025-10-05 13:17:58 +0530

    here i create nested route

---
+++ src/app/about/team-delhi/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TeamDelhi = () => {
+  return (
+    <div>
+      <h2>It is the delhi team</h2>
+    </div>
+  );
+};
+
+export default TeamDelhi;
---
+++ src/app/about/team-noida/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TeamNoida = () => {
+  return (
+    <div>
+      <h2>It is the noida team</h2>
+    </div>
+  );
+};
+
+export default TeamNoida;
