7aaf39eb6c0bcb9f894a1ffdacca6850bbd4772d
reetuTheCoder <codewithreetu@gmail.com>
2025-10-06 12:09:32 +0530

    remove some code

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-     <h1>Just setup Next.Js</h1>
+     <h1>It is out Home page</h1>
     </div>
   );
 }
