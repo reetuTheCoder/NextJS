ce27c5db400c615652d80b7e75c38c8761d699cc
reetuTheCoder <codewithreetu@gmail.com>
2025-10-07 12:06:29 +0530

    what ever you type after /blog/.... it will show in the page.js also will show first latter in the captial

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,16 +1,25 @@
 import React from "react";
 
 const BlogPost = ({ params }) => {
+  const { slug } = params;
   return (
     <div>
-      <h1>Post: {params.slug}</h1>
-        <p>You are viewing the blog post with slug: <strong>my-first-post</strong></p>
+      {/* <h1>Post: {params.slug}</h1>
+      <p>
+        You are viewing the blog post with slug: <strong>my-first-post</strong>
+      </p> */}
 
+      <h1>
+        {slug
+          .split("-")
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ")}
+      </h1>
+      <p> Post slug: {slug}</p>
     </div>
   );
 };
 
 export default BlogPost;
-
 
 //http://localhost:3000/blog/my-first-post you will get you dynamic route like this
