fab5e49c1783d8cf2cf539071f7f0204e56b0c29
reetuTheCoder <codewithreetu@gmail.com>
2025-10-08 14:52:40 +0530

    add notes for you reetu

--- src/app/shop/[category]/[...subpath]/page.js
+++ src/app/shop/[category]/[...subpath]/page.js
@@ -8,3 +8,15 @@ export default function ShopPage({ params }) {
     </div>
   );
 }
+
+
+// in the [...subpath] 
+// you can added nested lavel route end less route
+// like - http://localhost:3000/shop/electronics/laptops/macbook/macbook-pro/macbook-pro11
+// shop - first route
+// electronics - category route
+// subpath - laptops route
+// subpath - macbook route
+// subpath - macbook-pro/ route
+// subpath - macbook-pro11 route
+// you can clearly see how many routes we have create in the  [...subpath]  so it avoid nested folders
